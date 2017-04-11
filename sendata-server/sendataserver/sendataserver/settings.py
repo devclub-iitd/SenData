221dd3d266e488a554c6ac0d22cb0b5e96efb89f
Sumit Ghosh <sumit.ghosh32@gmail.com>
2017-04-11 22:59:19 +0530

    Changed settings.py according to the project.

--- sendata-server/sendataserver/sendataserver/settings.py
+++ sendata-server/sendataserver/sendataserver/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
+    'restapi',
 ]
 
 MIDDLEWARE = [
@@ -118,3 +120,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
+
+
+REST_FRAMEWORK = {
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.IsAdminUser',
+    ],
+    'PAGE_SIZE': 10
+}
