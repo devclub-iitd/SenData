851a2d149c4d99ca7026102c78316653d69238f0
ankurshaswat <ankurshaswat@yahoo.co.in>
2017-06-01 12:34:20 +0530

    node command running,included static files , commented out code, to be uncommented bit by bit with proper testing

--- js/main.js
+++ js/main.js
@@ -1,191 +1,208 @@
-$(function () {
+$(function() {
 
-    // implementing username form in a different way like socket.io
-    var $mainContent = $('.main-content');      // which contains userlist and search functionality
-    var username = '';      // variable to store username entered.
-    var $usernameInput = $('.usernameInput'); // Input for username
-    var $loginPage = $('.login-page');      // the login form area
-    var $window = $(window);
-    var $homePage = $('.home-page');        // home page
-    var $transferPage = $('.transfer-page');       // file transfer page
-    var $connectionRequest = $('#connection_request');          // modal to accept or deny a connection
-    var $acceptConnection = $connectionRequest.find('.btn-success');          // accept button in that modal
-    var $rejectConnection = $connectionRequest.find('.btn-danger');           // reject button in that modal
-	//var app = require('express')();
-	//var io = require('socket.io')(http);
-	
-    // Prevents input from having injected markup
-    function cleanInput(input) {
-        return $('<div/>').text(input).text();
+  // implementing username form in a different way like socket.io
+  var $mainContent = $('.main-content'); // which contains userlist and search functionality
+  var username = ''; // variable to store username entered.
+  var $usernameInput = $('.usernameInput'); // Input for username
+  var $loginPage = $('.login-page'); // the login form area
+  var $window = $(window);
+  var $homePage = $('.home-page'); // home page
+  var $transferPage = $('.transfer-page'); // file transfer page
+  var $connectionRequest = $('#connection_request'); // modal to accept or deny a connection
+  var $acceptConnection = $connectionRequest.find('.btn-success'); // accept button in that modal
+  var $rejectConnection = $connectionRequest.find('.btn-danger'); // reject button in that modal
+  var socket = io();
+  var page_number = 0;
+
+  alert("Running");
+
+  /*
+   page_number
+
+   case:0      page showing online user_names
+   case:1      page/popup showing waiting for permission
+   (other buttons should not be accesible during this time)
+   case:2      page of send
+
+
+   */
+  //var app = require('express')();
+  //var io = require('socket.io')(http);
+
+  // Prevents input from having injected markup
+  function cleanInput(input) {
+    return $('<div/>').text(input).text();
+  }
+
+  function setUsername() {
+    username = cleanInput($usernameInput.val().trim()); // trim is to remove extra blank spaces
+    // If the username is valid
+    if (username) {
+      // console.log(username);
+      $loginPage.hide();
+      $mainContent.show();
+      $loginPage.off('click');
     }
+  }
 
-    function setUsername() {
-        username = cleanInput($usernameInput.val().trim());         // trim is to remove extra blank spaces
-        // If the username is valid
-        if (username) {
-            // console.log(username);
-            $loginPage.hide();
-            $mainContent.show();
+  $window.keydown(function(event) {
+    // When the client hits ENTER on their keyboard
 
-            $loginPage.off('click');
-        }
+    if (event.which === 13) {
+      event.preventDefault();
+      socket.emit('login', username); //This sends a request to login with certain username
+
+      socket.on('login', function(result) { //this waits for a reply from the server after checking that the given username is available or not......result is a boolean
+
+        // here if result is true setUsername otherwise ask again.......
+
+
+        // if (!result) {
+        //   setUsername();
+        // } else {
+        //   $loginPage.fadeOut();
+        //   $mainContent.show();
+        //   $loginPage.off('click');
+        // }
+      });
+
     }
-
-    $window.keydown(function (event) {
-        // When the client hits ENTER on their keyboard
-
-        if (event.which === 13) {
-            event.preventDefault();
-            if (!username) {
-                setUsername();
-            }
-            else {
-                $loginPage.fadeOut();
-                $mainContent.show();
-                $loginPage.off('click');
-            }
-        }
-    });
-    // if user accepts the connection show the transfer page
-    $acceptConnection.click(function () {
-       $homePage.fadeOut();
-       $transferPage.show();
-    });
-    // else show the homePage
-    $rejectConnection.click(function () {
-        $connectionRequest.fadeOut();
-    });
-    var socket = io();
-    var page_number = 0;
-    
-    
-
-    /*
-     page_number
-
-     case:0      page showing online user_names
-     case:1      page/popup showing waiting for permission
-     (other buttons should not be accesible during this time)
-     case:2      page of send
+  });
+  // if user accepts the connection show the transfer page
+  $acceptConnection.click(function() {
+    $homePage.fadeOut();
+    $transferPage.show();
+  });
+  // else show the homePage
+  $rejectConnection.click(function() {
+    $connectionRequest.fadeOut();
+  });
 
 
-     */
-    socket.emit('login', username);
-    var flag = 0;
 
-	
-    socket.on("update list", function (connected_clients) {
-        // connected_clients is a dictionary
-        // use the keys to update list of user_names
-        // if the user is on page_number =0 ;
-    });
 
-	
-	
-	    var configuration = {		//Needed for RTCPeerConnection
-  'iceServers': [{
-    'url': 'stun:stun.example.org'
-		}]
-	};
-	var pc;		//variable to store the RTCPeerConnection object
-	var ExchangerUsername; //variable for name of requested username
+  var flag = 0;
 
-	// call start() to initiate peer connection process(should be called once 'Y' answer has been received (or sent))
-	function start() {
-	  myPeerConn = new RTCPeerConnection(configuration);
+  socket.on("update list", function(connected_clients) {
+    // connected_clients is a dictionary
+    // use the keys to update list of user_names
+    // if the user is on page_number =0 ;
+  });
 
-	  // send any ice candidates to the other peer
-	  myPeerConn.onicecandidate = function (evt) {
-		if (evt.candidate)
-		  socket.emit("candidate", {username:ExchangerUsername,candidate:evt.candidate});
-	  };
-	}
-	
-	function sendLocalDesc() {  //send local description to ExchangerUsername
-	  myPeerConn.createOffer().then(function(offer) {
-		return myPeerConnection.setLocalDescription(offer);
-	  })
-	  .then(function() {
-		socket.emit("session-desc",{              
-		  target: ExchangerUsername,
-		  type:"file-stream",						//not sure what should go here		
-		  sdp: myPeerConnection.localDescription
-		});
-	  })
-	  .catch(function(reason) {
-		// An error occurred, so handle the failure to connect
-	  });
-	  
-	  
-	}
-	
-	// call SendOffer when any username is clicked and
-    // also in the meantime show the screen that
-    // waiting for permission of user
-    // page_number=1
-	
-    // save name of requested user as ExhangerUsername
-    
-    function SendOffer(user){
+
+
+  var configuration = { //Needed for RTCPeerConnection
+    'iceServers': [{
+      'url': 'stun:stun.example.org'
+    }]
+  };
+
+  var pc; //variable to store the RTCPeerConnection object
+  var ExchangerUsername; //variable for name of requested username
+
+  // call start() to initiate peer connection process(should be called once 'Y' answer has been received (or sent))
+
+  function start() {
+    myPeerConn = new RTCPeerConnection(configuration);
+
+    // send any ice candidates to the other peer
+    myPeerConn.onicecandidate = function(evt) {
+      if (evt.candidate)
+        socket.emit("candidate", {
+          username: ExchangerUsername,
+          candidate: evt.candidate
+        });
+    };
+  }
+
+  function sendLocalDesc() { //send local description to ExchangerUsername
+    myPeerConn.createOffer().then(function(offer) {
+        return myPeerConnection.setLocalDescription(offer);
+      })
+      .then(function() {
+        socket.emit("session-desc", {
+          target: ExchangerUsername,
+          type: "file-stream", //not sure what should go here
+          sdp: myPeerConnection.localDescription
+        });
+      })
+      .catch(function(reason) {
+        // An error occurred, so handle the failure to connect
+      });
+  }
+
+  // call SendOffer when any username is clicked and
+  // also in the meantime show the screen that
+  // waiting for permission of user
+  // page_number=1
+
+  // save name of requested user as ExhangerUsername
+
+  function SendOffer(user) {
     socket.emit("offer", user);
-    ExchangerUsername=user
-	}
+    ExchangerUsername = user
+  }
 
-    socket.on("answer", function (answer) {
-        // if answer is yes.....goto page_number=2
-        // rest code follows.................
-        if (answer=="y"){
-			start(); //start the peerconnection process
-			sendLocalDesc();//create peer connection offer and send local description on other side
-		}
-		else{
-			ExchangerUsername=None;		// else set ExchangeUsername to None
-		}
-    });
+  socket.on("answer", function(answer) {
+    // if answer is yes.....goto page_number=2
+    // rest code follows.................
+    if (answer == "y") {
+      start(); //start the peerconnection process
+      sendLocalDesc(); //create peer connection offer and send local description on other side
+    } else {
+      ExchangerUsername = None; // else set ExchangeUsername to None
+    }
+  });
 
 
-    socket.on("offer", function (username) {
-        // show that username wants to connect to you
-        // accept or deny
-        // append as feed on the side//IE ANOTHER MODAL
-        //CHANGE THAT MODAL'S CSS DISPLAY ATTRIBUTE FROM HIDDEN TO BLOCK OR SOMETHING
-		
-		if (answer=="Y"){
-			start();
-			ExchangerUsername=username;
-			socket.emit("answer",{answer:answer,username:username})
-			// redirect to page 2
-		}
+  socket.on("offer", function(username) {
+    // show that username wants to connect to you
+    // accept or deny
+    // append as feed on the side//IE ANOTHER MODAL
+    //CHANGE THAT MODAL'S CSS DISPLAY ATTRIBUTE FROM HIDDEN TO BLOCK OR SOMETHING
+
+    if (answer == "Y") {
+      start();
+      ExchangerUsername = username;
+      socket.emit("answer", {
+        answer: answer,
+        username: username
+      })
+
+      $homePage.fadeOut();
+      $transferPage.show();
+      // redirect to page 2
+    }
+  });
+
+  socket.on("session-desc", function(message) {
+    myPeerConn.setRemoteDescription(message.sdp).then(function() {
+      if (myPeerConn.remoteDescription.type == 'offer') {
+        myPeerConn.createAnswer().then(function(answer) {
+            return myPeerConn.setLocalDescription(answer);
+          })
+          .then(function() {
+            socket.emit("session-desc", {
+              target: ExchangerUsername,
+              type: "file-stream",
+              sdp: myPeerConnection.localDescription
+            });
+          })
+          .catch(function(reason) {
+            // An error occurred, so handle the failure to connect
+          });
+      }
     })
+  });
 
-	socket.on("session-desc",function(message){
-		myPeerConn.setRemoteDescription(message.sdp).then(function(){
-			if (myPeerConn.remoteDescription.type=='offer'){
-				myPeerConn.createAnswer().then(function(answer) {
-					return myPeerConn.setLocalDescription(answer);
-				})
-				.then(function(){
-					socket.emit("session-desc",{             
-						target: ExchangerUsername,
-						type:"file-stream",
-						sdp: myPeerConnection.localDescription
-					});
-				})
-				.catch(function(reason) {
-				// An error occurred, so handle the failure to connect
-				});
-			}
-		})
-	});
+  socket.on("candidate", function(candidate) {
+    myPeerConn.addIceCandidate(candidate) //add remote icecandidate
+      .then(function() {
+        console.log('AddIceCandidate success.');
+      })
+      .catch(function() {
+        console.log('Error in adding IceCandidate');
+      });
+  });
 
-	socket.on("candidate",function(candidate){
-	myPeerConn.addIceCandidate(candidate)			//add remote icecandidate
-	.then(function() {
-	   console.log('AddIceCandidate success.');
-	})
-	.catch(function(){
-		console.log('Error in adding IceCandidate');
-	});
-		
-	});
 });
--- server.js
+++ server.js
@@ -1,85 +1,92 @@
-var app = require('express')();
+var express = require('express'); // Get the module
+var app = express(); // Create express by calling the prototype in var express
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 var connected_clients = [];
 //var soc_room = {};
-app.get('/', function (req, res) {
-    res.sendFile(__dirname + '/index.html');
+app.get('/', function(req, res) {
+  res.sendFile(__dirname + '/index.html');
+});
+app.use('/css', express.static('css'));
+app.use('/fonts', express.static('fonts'));
+app.use('/images', express.static('images'));
+app.use('/js', express.static('js'));
+
+
+
+
+io.on('connection', function(socket) {
+
+  socket.on("login", function(username) {
+    var idx = connected_clients.indexOf(username);
+    if (idx == -1) {
+      connected_clients[username] = socket.id;
+      socket.username = username;
+      console.log(username + " joined");
+      socket.emit("login", true);
+      io.emit("update list", connected_clients)
+
+    } else {
+      socket.emit("login", false);
+      //ask user for another username
+    }
+  });
+
+  //   socket.on("offer", function (username) { //currently not passed offer
+  //       // remove socket.username form connected_clients
+  //       // then emit list again
+  //       console.log("Sending offer to: ", username);
+  //       var user = connected_clients[username];
+  //       if (user != null) {
+  //           socket.partner = username;
+  //           socket.broadcast.to(user).emit("offer", socket.username);//I THINK WE SHOULD NOT USE THE KEYWORK OFFER IE THE SAME THING AS THAT OF CLIENT MAKING AN OFFER IT SHOULD BE "OFFERgoingToPartner"
+  //
+  //           //TO SEND SOMETHING TO BOTH THE USERS IE ONE GETS THE ACCEPT REJECT MODAL
+  //           //THE OTHER GETS THE WAITING FOR CONFRIRMATION MODAL
+  //       }
+  //       //TO WRITE AN ELSE FUNCTIONLITY IE IF THE USERNAME IS NOT IN  THE LATEST LIST ANYMORE...
+  //       //THEN TO SEND A SORRY MESSAGE
+  //   });
+  //   socket.on("answer", function (msg) {
+  // 	username=msg.username;
+  // 	answer=msg.answer;
+  //       console.log("Sending answer to: ", username);
+  //       // if answer is no add the username to connected_clients
+  //       // then emit list again
+  //       var user = connected_clients[username];
+  //       if (user != null) {
+  //           socket.partner = username;
+  //           socket.broadcast.to(user).emit("answer", answer);
+  //       }
+  //   });
+  //
+  //   socket.on("candidate", function (msg) {
+  // 	username=msg.username;
+  // 	candidate=msg.candidate;
+  //       console.log("Sending candidate to: ", username);
+  //       var user = connected_clients[username];
+  //       if (user != null) {
+  //           socket.partner = username;
+  //           socket.broadcast.to(user).emit("candidate", candidate);
+  //       }
+  //   });
+  //
+  // socket.on("session-desc",function (msg) {
+  // 	username=msg.target;
+  // 	var user=connected_clients
+  // 	socket.broadcast.to(user).emit("session-desc",msg);
+  // });
+  //
+  //   socket.on("disconnect", function () {
+  //       if (socket.partner) {
+  //           if (socket.partner in connected_clients) {
+  //               socket.broadcast.to(connected_clients[socket.partner]).emit("Partner disconnected");
+  //           }
+  //       }
+  //       delete connected_clients[socket.username];
+  //   });
 });
 
-
-
-
-io.on('connection', function (socket) {
-    socket.on("login", function (username) {
-        var idx = connected_clients.indexOf(username);
-        if (idx == -1) {
-            connected_clients[username] = socket.id;
-            socket.username = username;
-            socket.emit("login", true);
-            io.emit("update list", connected_clients)
-        }
-        // else {//not needed now as not implemented a handler on client side
-        //   socket.emit("login", false);
-        // }
-    });
-    socket.on("offer", function (username) { //currently not passed offer
-        // remove socket.username form connected_clients
-        // then emit list again
-        console.log("Sending offer to: ", username);
-        var user = connected_clients[username];
-        if (user != null) {
-            socket.partner = username;
-            socket.broadcast.to(user).emit("offer", socket.username);//I THINK WE SHOULD NOT USE THE KEYWORK OFFER IE THE SAME THING AS THAT OF CLIENT MAKING AN OFFER IT SHOULD BE "OFFERgoingToPartner"
-
-            //TO SEND SOMETHING TO BOTH THE USERS IE ONE GETS THE ACCEPT REJECT MODAL
-            //THE OTHER GETS THE WAITING FOR CONFRIRMATION MODAL
-        }
-        //TO WRITE AN ELSE FUNCTIONLITY IE IF THE USERNAME IS NOT IN  THE LATEST LIST ANYMORE...
-        //THEN TO SEND A SORRY MESSAGE
-    });
-    socket.on("answer", function (msg) {
-		username=msg.username;
-		answer=msg.answer;
-        console.log("Sending answer to: ", username);
-        // if answer is no add the username to connected_clients
-        // then emit list again
-        var user = connected_clients[username];
-        if (user != null) {
-            socket.partner = username;
-            socket.broadcast.to(user).emit("answer", answer);
-        }
-    });
-
-    socket.on("candidate", function (msg) {
-		username=msg.username;
-		candidate=msg.candidate;
-        console.log("Sending candidate to: ", username);
-        var user = connected_clients[username];
-        if (user != null) {
-            socket.partner = username;
-            socket.broadcast.to(user).emit("candidate", candidate);
-        }
-    });
-	
-	socket.on("session-desc",function (msg) {
-		username=msg.target;
-		var user=connected_clients
-		socket.broadcast.to(user).emit("session-desc",msg);
-	});
-	
-    socket.on("disconnect", function () {
-        if (socket.partner) {
-            if (socket.partner in connected_clients) {
-                socket.broadcast.to(connected_clients[socket.partner]).emit("Partner disconnected");
-            }
-        }
-        delete connected_clients[socket.username];
-    });
+http.listen(3001, '0.0.0.0', function() {
+  console.log('listening on *:3001');
 });
-http.listen(3001, '0.0.0.0', function () {
-    console.log('listening on *:3001');
-});
-
-
-
