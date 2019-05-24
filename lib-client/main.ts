bab725c3f897abe2d900bbdf99abd0d4c7a10e5f
Arpit Saxena <arpit.saxena2000@yahoo.in>
2019-05-24 15:58:53 +0530

    Added testing code
    - Since we're working on later workflow, a very simple way of
      connecting users has been used
    - Any two users who open the page first are connected, rest are
      disconnected

--- lib-client/main.ts
+++ lib-client/main.ts
@@ -1,13 +1,15 @@
-"use strict";
 $(() => {
     const socket = io();
+
     socket.on('bye-bye', () => {
         $('body').text('Server sent bye-bye');
     });
+
     socket.on('connected', () => {
         $('body').text('Connected to another user!');
-    });
+    })
+
     socket.on('disconnected', () => {
         $('body').text('The other user has disconnected!');
-    });
+    })
 });
--- lib/server.ts
+++
@@ -1,8 +0,0 @@
-import http = require('http');
-import express = require('./express');
-import env = require('./env');
-
-const app: Express.Application = express();
-const server: http.Server = new http.Server(app);
-
-server.listen(env.PORT);
