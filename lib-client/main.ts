660e404d657f3a57104f903a7ca7f36ad455a9c9
Vishal Singh <tmibvishal@gmail.com>
2019-05-26 09:41:04 +0530

    add ts lint

--- lib-client/main.ts
+++ lib-client/main.ts
@@ -1,15 +1,14 @@
 $(() => {
     const socket = io();
-
     socket.on('bye-bye', () => {
         $('body').text('Server sent bye-bye');
     });
 
     socket.on('connected', () => {
         $('body').text('Connected to another user!');
-    })
+    });
 
     socket.on('disconnected', () => {
         $('body').text('The other user has disconnected!');
-    })
+    });
 });
