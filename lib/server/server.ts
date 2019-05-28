7c80d26cc44f5d5c8d0722c81afaec0e50dfc373
Anweshan Bor <tt1180878@iitd.ac.in>
2019-05-28 09:55:58 +0530

    connection part initiated

--- lib/server/server.ts
+++ lib/server/server.ts
@@ -2,45 +2,132 @@ import http = require('http');
 import express = require('./express');
 import env = require('./env');
 import socketIO = require('socket.io');
-import webTorrent = require('webtorrent')
 
 const app: Express.Application = express();
 const server: http.Server = new http.Server(app);
 const io: socketIO.Server= socketIO(server);
+let status: number;
 
 server.listen(env.PORT, () => {
     console.log(`listening on *:${env.PORT}`);
 });
 
-let numUsers: number = 0;
-let user1: string, user2: string;
+// creating interface for different characteristics of a logged user
+interface User {
+    uname: string,
+    state: string,
+    outRequest: string,
+    partner: string,
+    inRequests: Set<string>
+}
+
+// declaring a user map containing all users mapped from their soscketids to their characteristics.
+let users: Map<string, UserCh> = new Map();
+
+
 
 io.on('connection', (socket: socketIO.Socket) => {
-    if(numUsers >= 2) {
-        socket.emit('bye-bye');
-        socket.disconnect();
-        return;
-    }
+    // if(users.size >= 2) {
+    //     socket.emit('bye-bye');
+    //     socket.disconnect();
+    //     return;
+    // }
 
-    numUsers++;
 
-    if (numUsers == 2){
-        io.emit('connected')
-    }
 
-    socket.on('fileReady', (opts: { magnetURI: string }) => {
-        socket.broadcast.emit('addTorrent', opts);
-    })
+    // users.add(socket.id);
 
-    socket.on('downloadComplete', () => {
-        //TODO
+    // var val= <UserCh> {
+    //     socketID: socket.id,
+    //     state: "",
+    //     outRequest: "",
+    //     partner: "",
+    //     inRequests: {}
+    // } 
+
+    // if(users.size == 2) {
+    //     io.emit('connected');
+    // }
+
+    // login event
+    socket.on('login', (username: string)=>{
+        
+        // if username already exists in the user map
+        if(users.has(socket.id) || username === ''){
+            status = 1;
+        }
+        else{
+
+            //initialising characteristics for logged user(updatable later)
+            let val= <UserCh> {
+                uname: username,
+                state: "idle",
+                outRequest: "",
+                partner: "",
+                inRequests: {}
+            }
+
+            //mapping logged user to its characteristic values.
+            users.set(socket.id, val);
+
+            // confirming user that its logged in
+            status = 0;
+            socket.emit('login', status);
+        }
     });
 
     socket.on('disconnect', () => {
-        if(numUsers == 2){
-            io.emit('disconnected');
+        // if(users.size == 2){
+        //     io.emit('disconnected');
+        // }
+        // disconnected user characteristics
+        let checkval = <UserCh> users.get(socket.id);
+
+        // if current disconnected user was paired to some user
+        if(checkval.partner !== ""){
+
+            //characteristics of partner
+            let changeval = <UserCh> users.get(checkval.partner);
+            
+            // let toval = <UserCh> {
+            //     uname: fromval.uname,
+            //     state: "idle",
+            //     outRequest: "",
+            //     partner: "",
+            //     inRequests: fromval.inRequests
+            // }
+
+            changeval.state = "idle";
+            changeval.outRequest = "";
+            changeval.partner = "";
+
+            //characteristics of partner updated
+            users.set(checkval.partner, changeval);
+
+            //message sent to partner
+            socket.broadcast.to(checkval.partner).emit(`${checkval.uname} disconnected`);
         }
 
-        numUsers--;
-    })
+        // looping user map for all other users waiting for this socket to reply yo their request
+        for(let key of users.keys()){
+
+            //getting characteristics
+            let waitval = <UserCh> users.get(key);
+
+            if(waitval.partner === socket.id){
+                
+                //updating characteristics
+                waitval.outRequest = "";
+
+                users.set(key, waitval);
+
+                socket.broadcast.to(key).emit(`${checkval.uname} disconnected`);
+            }
+
+        }
+
+        //deleted socket
+        users.delete(socket.id);
+        // users.delete(socket.id);
+    });
 });
