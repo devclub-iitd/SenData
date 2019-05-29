2295ac58710ff2b5d4dadfe5fdbfc86b316403a1
Vishal Singh <tmibvishal@gmail.com>
2019-05-30 01:11:53 +0530

    add socket event message in chatbox client .ts file

--- lib/server/chatbot_server.ts
+++ lib/server/chatbot_server.ts
@@ -7,20 +7,34 @@ const server: http.Server = new http.Server(app);
 const io: socketIO.Server = socketIO(server);
 
 class Msg {
+    public username: string;
     public messageValue: string;
     public timeStamp: string;
-    constructor(messageValue: string) {
+    constructor(username: string, messageValue: string) {
+        this.username = username;
         this.messageValue = messageValue;
         const currentdate = new Date();
-        this.timeStamp = currentdate.getHours() + ":"
-                        + currentdate.getMinutes() + ":"
+        this.timeStamp = currentdate.getHours() + ':'
+                        + currentdate.getMinutes() + ':'
                         + currentdate.getSeconds();
     }
 }
 
 io.on('connection', (socket) => {
     socket.on('message', (messageValue: string) => {
-        const msg = new Msg(messageValue);
-        socket.emit('message', msg);
+        // Getting the user1
+        const username1 = socket.username;
+        const user1: User = users.get(username1) as User;
+        if (user1.state === 'connected') {
+            // Creating the new msg using the class Msg
+            const msg = new Msg(username1, messageValue);
+            // Getting the user2 (partner of user1)
+            const username2 = user1.partner;
+            const user2: User = users.get(username2) as User;
+            // Emitting the msg to user1 (sender)
+            socket.emit('message', msg);
+            // Broadcasting the msg to user2 only
+            socket.broadcast.to(user2.socketID).emit('message', msg);
+        }
     });
   });
