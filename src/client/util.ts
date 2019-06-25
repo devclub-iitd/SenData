599231728e7b2c5c03796068fd2d84ad2b3369da
Arpit Saxena <arpit.saxena2000@yahoo.in>
2019-06-25 20:22:57 +0530

    Fixed more linting issues

--- src/client/util.ts
+++ src/client/util.ts
@@ -1,29 +1,29 @@
 // Taken from https://stackoverflow.com/a/18650828/5585431
-export function formatBytes(bytes: number, decimals = 2) {
-    if (bytes === 0) {
-        return "0 Bytes";
-    }
+export function formatBytes(bytes: number, decimals = 2): string {
+  if (bytes === 0) {
+    return "0 Bytes";
+  }
 
-    const k = 1024;
-    const dm = decimals < 0 ? 0 : decimals;
-    const sizes = ["Bytes", "KB", "MB", "GB", "TB", "PB", "EB", "ZB", "YB"];
+  const k = 1024;
+  const dm = decimals < 0 ? 0 : decimals;
+  const sizes = ["Bytes", "KB", "MB", "GB", "TB", "PB", "EB", "ZB", "YB"];
 
-    const i = Math.floor(Math.log(bytes) / Math.log(k));
+  const i = Math.floor(Math.log(bytes) / Math.log(k));
 
-    return parseFloat((bytes / Math.pow(k, i)).toFixed(dm)) + " " + sizes[i];
+  return parseFloat((bytes / Math.pow(k, i)).toFixed(dm)) + " " + sizes[i];
 }
 
-export function formatTime(time /*in ms*/: number) {
-    time /= 1000;
-    const sec = Math.round(time % 60);
-    time /= 60;
-    const min = Math.round(time % 60);
-    time /= 60;
-    const hour = Math.round(time);
+export function formatTime(time: number): string {
+  time /= 1000;
+  const sec = Math.round(time % 60);
+  time /= 60;
+  const min = Math.round(time % 60);
+  time /= 60;
+  const hour = Math.round(time);
 
-    let text = "";
-    if (hour !== 0) { text += `${hour} h `; }
-    if (min !== 0) { text += `${min} m `; }
-    if (sec !== 0) { text += `${sec} s `; }
-    return text;
+  let text = "";
+  if (hour !== 0) { text += `${hour} h `; }
+  if (min !== 0) { text += `${min} m `; }
+  if (sec !== 0) { text += `${sec} s `; }
+  return text;
 }
--- src/client/wt.ts
+++ src/client/wt.ts
@@ -35,13 +35,13 @@ const debug = debugLib("FileSend-WebTorrent");
 *   - **clientDestroyed** (WebTorrent client was destroyed, possibly due to some error)
 */
 export default class Client extends EventEmitter {
-    public fileNames: string[] | undefined;
-    private readonly socket: SocketIOClient.Socket;
-    private readonly TRACKER_URLS: string[];
-    private readonly client: WebTorrent.Instance;
-    private readonly TIME_INTERVAL = 500; // Interval to report progress (ms)
+  public fileNames: string[] | undefined;
+  private readonly socket: SocketIOClient.Socket;
+  private readonly TRACKER_URLS: string[];
+  private readonly client: WebTorrent.Instance;
+  private readonly TIME_INTERVAL = 500; // Interval to report progress (ms)
 
-    /*
+  /*
     * Assuming use of SocketIO for comm. to server.
     * socket is the socket object of client connected.
     *
@@ -49,211 +49,217 @@ export default class Client extends EventEmitter {
     * for setting. If not set, falls back to third party
     * defaults.
     */
-    constructor(socket: SocketIOClient.Socket) {
-        super();
-        let STUN_URL: string;
-        let TRACKER_URL: string;
+  public constructor(socket: SocketIOClient.Socket): void {
+    super();
+    let STUN_URL: string;
+    let TRACKER_URL: string;
 
-        if (process.env.STUN_URL) {
-            STUN_URL = `stun:${process.env.STUN_URL}:3478`;
-        } else {
-            STUN_URL = "stun:stun.l.google.com:19302";
-            debug(`STUN_URL env variable not set`);
-        }
-        debug(`Using ${STUN_URL} as STUN server address`);
-
-        if (process.env.TRACKER_URL) {
-            TRACKER_URL = `ws://${process.env.TRACKER_URL}:8000`;
-        } else {
-            TRACKER_URL = `wss://tracker.btorrent.xyz`;
-            debug("TRACKER_URL env variable not set");
-        }
-        debug(`Using ${TRACKER_URL} as TRACKER address`);
-
-        this.client = new WebTorrent({
-            tracker: {
-                iceServers: [{ urls: STUN_URL }],
-            },
-        });
-
-        this.TRACKER_URLS = [ TRACKER_URL ];
-
-        this.socket = socket;
-        this.socket.on("addTorrent", (magnetUri: string) => {
-            this.addTorrent(magnetUri);
-        });
-
-        // Fatal errors, client is destroyed after encounter
-        this.client.on("error", (err) => {
-            debug(`WebTorrent client encountered an error: ${err}`);
-            this.emit("error", err);
-            this.emit("clientDestroyed");
-        });
+    if (process.env.STUN_URL) {
+      STUN_URL = `stun:${process.env.STUN_URL}:3478`;
+    } else {
+      STUN_URL = "stun:stun.l.google.com:19302";
+      debug(`STUN_URL env variable not set`);
     }
+    debug(`Using ${STUN_URL} as STUN server address`);
 
-    public sendFiles = (files: File | File[] | FileList ) => {
-        this.client.seed(files, { announce: this.TRACKER_URLS } , (torrent) => {
-            this.setTorrentErrorHandlers(torrent);
-            this.setFileNames(torrent);
-            this.emit("seeding");
-            this.socket.emit("fileReady", torrent.magnetURI);
-
-            this.socket.on("progressUpdate", (info: { [key: string]: any }) => {
-                info.uploaded = formatBytes(torrent.uploaded);
-                info.uploadSpeed = formatBytes(torrent.uploadSpeed) + "/s";
-                this.emit("uploadProgress", info);
-            });
-
-            torrent.on("upload", (bytes) => {
-                this.emit("upload", {
-                    uploadSpeed: formatBytes(torrent.uploadSpeed) + "/s",
-                    uploaded: formatBytes(torrent.uploaded),
-                });
-            });
-
-            this.socket.on("torrentDone", (magnetUri: string) => {
-                torrent.destroy(() => {
-                    debug("Torrent destroyed, as server sent torrentDone");
-                    this.emit("torrentDestroyed");
-                });
-            });
-        });
+    if (process.env.TRACKER_URL) {
+      TRACKER_URL = `ws://${process.env.TRACKER_URL}:8000`;
+    } else {
+      TRACKER_URL = `wss://tracker.btorrent.xyz`;
+      debug("TRACKER_URL env variable not set");
     }
+    debug(`Using ${TRACKER_URL} as TRACKER address`);
 
-    /*
+    this.client = new WebTorrent({
+      tracker: {
+        iceServers: [{ urls: STUN_URL }],
+      },
+    });
+
+    this.TRACKER_URLS = [ TRACKER_URL ];
+
+    this.socket = socket;
+    this.socket.on("addTorrent", (magnetUri: string): void => {
+      this.addTorrent(magnetUri);
+    });
+
+    // Fatal errors, client is destroyed after encounter
+    this.client.on("error", (err): void => {
+      debug(`WebTorrent client encountered an error: ${err}`);
+      this.emit("error", err);
+      this.emit("clientDestroyed");
+    });
+  }
+
+  public sendFiles = (files: File | File[] | FileList ): void => {
+    this.client.seed(files, { announce: this.TRACKER_URLS } , (torrent): void => {
+      this.setTorrentErrorHandlers(torrent);
+      this.setFileNames(torrent);
+      this.emit("seeding");
+      this.socket.emit("fileReady", torrent.magnetURI);
+
+      this.socket.on("progressUpdate", (info: {
+        progress: number;
+        progressFiles: number[];
+        timeRemaining: number;
+        uploaded: string;
+        uploadSpeed: string;
+      }): void => {
+        info.uploaded = formatBytes(torrent.uploaded);
+        info.uploadSpeed = formatBytes(torrent.uploadSpeed) + "/s";
+        this.emit("uploadProgress", info);
+      });
+
+      torrent.on("upload", (bytes): void => {
+        this.emit("upload", {
+          uploadSpeed: formatBytes(torrent.uploadSpeed) + "/s",
+          uploaded: formatBytes(torrent.uploaded),
+        });
+      });
+
+      this.socket.on("torrentDone", (magnetUri: string): void => {
+        torrent.destroy((): void => {
+          debug("Torrent destroyed, as server sent torrentDone");
+          this.emit("torrentDestroyed");
+        });
+      });
+    });
+  }
+
+  /*
     * Expects an array of booleans to select and deselect files to download
     * E.g. Considering a torrent with 3 files, passing [true, false, true]
     * would result in first and third being selected, and second deselected
     */
-    public selectFiles = (fileSelections: boolean[]) => {
-        const torrent = this.client.torrents[0]; // Only one torrent is being used.
-        if (fileSelections.length !== torrent.files.length) {
-            debug(`Got ${fileSelections.length} length of choices, expected ${torrent.files.length}`);
-            debug("Ignored file selection choices");
+  public selectFiles = (fileSelections: boolean[]): void => {
+    const torrent = this.client.torrents[0]; // Only one torrent is being used.
+    if (fileSelections.length !== torrent.files.length) {
+      debug(`Got ${fileSelections.length} length of choices, expected ${torrent.files.length}`);
+      debug("Ignored file selection choices");
+    } else {
+      torrent.deselect(0, torrent.pieces.length - 1, 0);
+      for (let i = 0; i < torrent.files.length; i++) {
+        const file = torrent.files[i];
+        if (fileSelections[i]) {
+          file.select();
         } else {
-            torrent.deselect(0, torrent.pieces.length - 1, 0);
-            for (let i = 0; i < torrent.files.length; i++) {
-                const file = torrent.files[i];
-                if (fileSelections[i]) {
-                    file.select();
-                } else {
-                    file.deselect();
-                }
-            }
-            debug("Implemented file selection choices");
+          file.deselect();
         }
+      }
+      debug("Implemented file selection choices");
     }
+  }
 
-    /*
+  /*
     * Assumes all files of the torrent are to be downloaded as of now
     * Use selectFiles method to deselect files.
     */
-    public addTorrent = (magnetURI: string) => {
-        const torrent = this.client.add(magnetURI, { announce: this.TRACKER_URLS }, () => {
-            this.emit("downloading");
-        });
+  public addTorrent = (magnetURI: string): void => {
+    const torrent = this.client.add(magnetURI, { announce: this.TRACKER_URLS }, (): void => {
+      this.emit("downloading");
+    });
 
-        let downloadInfoInterval: number; // Unique ID of progress reporting interval
-        let progressFiles: number[];
+    let downloadInfoInterval: number; // Unique ID of progress reporting interval
+    let progressFiles: number[];
 
-        const onDownload = () => {
-            if (progressFiles === undefined) {
-                progressFiles = new Array(torrent.files.length);
-                for (let i = 0; i < progressFiles.length; i++) {
-                    progressFiles[i] = 0;
-                }
-            }
-
-            torrent.files.forEach( (file, i) => {
-                progressFiles[i] = file.progress;
-            });
-
-            this.emit("downloadProgress", {
-                downloadSpeed: formatBytes(torrent.downloadSpeed) + "/s",
-                downloaded: formatBytes(torrent.downloaded),
-                progress: torrent.progress,
-                progressFiles,
-                timeRemaining: torrent.timeRemaining,
-            });
-        };
-
-        torrent.on("metadata", () => {
-            progressFiles = new Array(torrent.files.length);
-            for (let i = 0; i < progressFiles.length; i++) {
-                progressFiles[i] = 0;
-            }
-            this.setFileNames(torrent);
-        });
-
-        torrent.on("ready", () => {
-            debug("Torrent beginning to download");
-
-            torrent.files.forEach( (file, i) => {
-                file.getBlob( (err, blob) => {
-                    if (err) {
-                        debug(`Obtaining file ${file.name}: ${err}`);
-                    } else if (blob === undefined) {
-                        debug(`Got undefined url for file ${file.name}`);
-                    } else {
-                        const url = URL.createObjectURL(blob);
-                        this.emit("fileDownloadComplete", {
-                            blob,
-                            index: i,
-                            url,
-                        });
-                    }
-                });
-            });
-
-            const socket = this.socket;
-            const sendProgress = () => {
-                socket.emit("progressUpdate", {
-                    progress: torrent.progress,
-                    progressFiles,
-                    timeRemaining: torrent.timeRemaining,
-                });
-            };
-
-            downloadInfoInterval = window.setInterval(() => {
-                sendProgress();
-                onDownload();
-            }, this.TIME_INTERVAL);
-
-            torrent.on("done", () => {
-                debug("Torrent download complete");
-                this.socket.emit("downloadComplete");
-                this.emit("downloadComplete");
-                clearInterval(downloadInfoInterval);
-                onDownload(); // To completely update download info
-                sendProgress();
-            });
-        });
-    }
-
-    // Should be used as client = client.destroy();
-    public destroy = () => {
-        this.client.destroy();
-        return null;
-    }
-
-    private setFileNames = (torrent: WebTorrent.Torrent) => {
-        this.fileNames = new Array(torrent.files.length);
-        for (let i = 0; i < torrent.files.length; i++) {
-            this.fileNames[i] = torrent.files[i].name;
+    const onDownload = (): void => {
+      if (progressFiles === undefined) {
+        progressFiles = new Array(torrent.files.length);
+        for (let i = 0; i < progressFiles.length; i++) {
+          progressFiles[i] = 0;
         }
-    }
+      }
 
-    private setTorrentErrorHandlers = (torrent: WebTorrent.Torrent) => {
-        // NOTE: torrents are destroyed when they encounter an error
-        torrent.on("error", (err) => {
-            debug(`Torrent encountered an error: ${err}`);
-            this.emit("error", err);
-            this.emit("torrentDestroyed");
-        });
+      torrent.files.forEach( (file, i): void => {
+        progressFiles[i] = file.progress;
+      });
 
-        // Warnings are not fatal, but useful for debugging
-        torrent.on("warning", (err) => {
-            debug(`Torrent warning: ${err}`);
+      this.emit("downloadProgress", {
+        downloadSpeed: formatBytes(torrent.downloadSpeed) + "/s",
+        downloaded: formatBytes(torrent.downloaded),
+        progress: torrent.progress,
+        progressFiles,
+        timeRemaining: torrent.timeRemaining,
+      });
+    };
+
+    torrent.on("metadata", (): void=> {
+      progressFiles = new Array(torrent.files.length);
+      for (let i = 0; i < progressFiles.length; i++) {
+        progressFiles[i] = 0;
+      }
+      this.setFileNames(torrent);
+    });
+
+    torrent.on("ready", (): void => {
+      debug("Torrent beginning to download");
+
+      torrent.files.forEach( (file, i): void => {
+        file.getBlob( (err, blob): void => {
+          if (err) {
+            debug(`Obtaining file ${file.name}: ${err}`);
+          } else if (blob === undefined) {
+            debug(`Got undefined url for file ${file.name}`);
+          } else {
+            const url = URL.createObjectURL(blob);
+            this.emit("fileDownloadComplete", {
+              blob,
+              index: i,
+              url,
+            });
+          }
         });
+      });
+
+      const socket = this.socket;
+      const sendProgress = (): void => {
+        socket.emit("progressUpdate", {
+          progress: torrent.progress,
+          progressFiles,
+          timeRemaining: torrent.timeRemaining,
+        });
+      };
+
+      downloadInfoInterval = window.setInterval((): void => {
+        sendProgress();
+        onDownload();
+      }, this.TIME_INTERVAL);
+
+      torrent.on("done", (): void => {
+        debug("Torrent download complete");
+        this.socket.emit("downloadComplete");
+        this.emit("downloadComplete");
+        clearInterval(downloadInfoInterval);
+        onDownload(); // To completely update download info
+        sendProgress();
+      });
+    });
+  }
+
+  // Should be used as client = client.destroy();
+  public destroy = (): null => {
+    this.client.destroy();
+    return null;
+  }
+
+  private setFileNames = (torrent: WebTorrent.Torrent): void => {
+    this.fileNames = new Array(torrent.files.length);
+    for (let i = 0; i < torrent.files.length; i++) {
+      this.fileNames[i] = torrent.files[i].name;
     }
+  }
+
+  private setTorrentErrorHandlers = (torrent: WebTorrent.Torrent): void => {
+    // NOTE: torrents are destroyed when they encounter an error
+    torrent.on("error", (err): void => {
+      debug(`Torrent encountered an error: ${err}`);
+      this.emit("error", err);
+      this.emit("torrentDestroyed");
+    });
+
+    // Warnings are not fatal, but useful for debugging
+    torrent.on("warning", (err): void => {
+      debug(`Torrent warning: ${err}`);
+    });
+  }
 }
